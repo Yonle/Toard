8d383a10abba6e58994f4c8e5bc38a5a63645b1b
Yonle <yonle@protonmail.com>
2021-12-18 13:07:45 +0700

    [Beta] Discovering feature

--- index.js
+++ index.js
@@ -15,36 +15,37 @@ a.use(bp());
 
 a.get("/", (q, s) => s.redirect("/hello_there"));
 a.post("/create", async (q, s) => {
-	let { t, d } = q.body;
-	if (!t || !d || !t.length || !d.length) return s.status(400).end("Invalid Body");
-	
-	let id = Math.floor(Math.random() * 10000000).toString();
+    let { t, d } = q.body;
+    if (!t || !d || !t.length || !d.length) return s.status(400).end("Invalid Body");
 
-	await db.push(id.toLowerCase(), { t, ts: Date.now(), d });
-	s.redirect("/" + id);
+    let id = Math.floor(Math.random() * 10000000).toString();
+
+    await db.push(id.toLowerCase(), { t, ts: Date.now(), d });
+    s.redirect("/" + id);
 });
 
 a.use("/:id", async (q, s, n) => {
-	if (q.params.id && await db.has(q.params.id.toLowerCase())) {
-		q.id = q.params.id.toLowerCase();
-		n();
-	} else s.status(400).end("Not found or deleted");
+    if (q.params.id && await db.has(q.params.id.toLowerCase())) {
+        q.id = q.params.id.toLowerCase();
+        n();
+    } else s.status(400).end("Not found or deleted");
 });
 
 a.get("/:id", async (q, s) => {
-	s.render("index.ejs", {
-		posts: await db.get(q.id), id: q.id
-	});
+    let trd = await db.all();
+    s.render("index.ejs", {
+        pst: trd[q.id], id: q.id, bds: await db.all()
+    });
 });
 
 a.post("/:id/reply", async (q, s) => {
-	if (q.id == "hello_there") return s.status(400).end("Post is unreplyable.");	
-	if (!(await db.has(q.id))) return s.status(404).end("Post is unavailable.");
+    if (q.id == "hello_there") return s.status(400).end("Post is unreplyable.");
+    if (!(await db.has(q.id))) return s.status(404).end("Post is unavailable.");
 
-	q.body.ts = Date.now();
-	await db.push(q.id, q.body);
+    q.body.ts = Date.now();
+    await db.push(q.id, q.body);
 
-	s.redirect(`/${q.id}#reply`);
+    s.redirect(`/${q.id}#reply`);
 });
 
 a.listen(process.env.PORT || 3000);
