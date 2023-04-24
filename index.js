43434e9e7d73a5f4a3fa60c9c4962eb20362f56a
Yonle <yonle@lecturify.net>
2023-04-24 12:03:58 +0700

    NewID new implementation was too long. Reduce
    Signed-off-by: Yonle <yonle@lecturify.net>

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ a.post("/create", async (q, s) => {
     if (q.ct && !q.wl)
       return c.newCaptchaSession(q, s, "create");
 
-    const id = (1000000 + ths - 2 + 1) + "_" + Date.now() + "_" + Math.random().toString(36).slice(2);
+    const id = Math.random().toString(36).slice(2) + "_" + (1000000 + ths - 2 + 1);
 
     db.exec(`CREATE TABLE '${id}' (ts INTEGER, t TEXT, d TEXT);`);
     db.prepare("INSERT OR IGNORE INTO __threadlists VALUES (?);").run(id.toString());
@@ -205,7 +205,7 @@ a.post("/verify", (q, s) => {
 
       try {
         if (sess.onid === "create") {
-          sess.onid = (1000000 + ths - 2 + 1) + "_" + Date.now() + "_" + Math.random().toString(36).slice(2);
+          sess.onid = Math.random().toString(36).slice(2) + "_" + (1000000 + ths - 2 + 1);
           db.exec(`CREATE TABLE '${sess.onid}' (ts INTEGER, t TEXT, d TEXT);`);
           ths++;
         }
