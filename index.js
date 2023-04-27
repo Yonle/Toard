49f2cbfffd002bb05e6e2210a50388664fd745e5
Yonle <yonle@lecturify.net>
2023-04-27 23:28:40 +0700

    Create isp block table if not exists
    Signed-off-by: Yonle <yonle@lecturify.net>

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ db.transaction(_ => {
 sys.exec("CREATE TABLE IF NOT EXISTS ip_block (ip TEXT, UNIQUE(ip));");
 sys.exec("CREATE TABLE IF NOT EXISTS ip_white (ip TEXT, UNIQUE(ip));");
 sys.exec("CREATE TABLE IF NOT EXISTS locked_thread (id TEXT, UNIQUE(id));");
+sys.exec("CREATE TABLE IF NOT EXISTS isp_block (name TEXT, UNIQUE(name));");
 sys.exec("CREATE TABLE IF NOT EXISTS config (name TEXT, value TEXT, UNIQUE(name));");
 
 let ths = db.prepare("SELECT id FROM __threadlists;").all().length;
