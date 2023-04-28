69e1339e59256944aae72d3dc0e9b8adb8dc56b6
Yonle <yonle@lecturify.net>
2023-04-28 23:00:26 +0700

    ports_scanner: Leave it scanning at behind.
    Signed-off-by: Yonle <yonle@lecturify.net>

--- ports_scanner.js
+++ ports_scanner.js
@@ -7,25 +7,32 @@ module.exports = sys => (q, s, n) => {
   const ib = sys.prepare("SELECT ip FROM ip_block WHERE ip = ?;");
   const bl = sys.prepare("SELECT port FROM blocked_open_ports;").all().map(i => i.port);
 
-  if (wl.get(ip) || ib.get(ip) || !bl.length) return n();
+  n();
+
+  if (wl.get(ip) || ib.get(ip) || !bl.length) return;
 
   const sock = new net.Socket();
+
+  let timeout = null;
   sock.on('error', _ => null);
   sock.on('close', _ => {
     if (!bl.length || !sock.destroyed) {
       sock.removeAllListeners('error');
       sock.removeAllListeners('close');
       sock.removeAllListeners('connect');
-      return n();
+      return;
     };
     sock.connect(bl.shift(), ip);
+    timeout = setTimeout(_ => sock.end(), 2000);
   });
 
   sock.on('connect', _ => {
+    clearTimeout(timeout);
     console.log(`Detected open port at [${ip}]:${sock.remotePort}. Adding to blacklist.`);
     sys.prepare("INSERT INTO ip_block VALUES (?);").run(ip);
     sock.destroy();
   });
 
   sock.connect(bl.shift(), ip);
+  timeout = setTimeout(_ => sock.end(), 2000);
 };
