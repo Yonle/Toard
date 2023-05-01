ab73e33db3746d2ed228816b8e5ccdf7b12e6bb6
Yonle <yonle@lecturify.net>
2023-05-01 11:36:16 +0700

    whois.js: UNIQUE(ip) to avoid duplicates
    Signed-off-by: Yonle <yonle@lecturify.net>

--- whois.js
+++ whois.js
@@ -2,16 +2,17 @@ const whois = require("whois");
 const sql = require("better-sqlite3");
 const cache = new sql("./whoiscache.db");
 
-cache.exec("CREATE TABLE IF NOT EXISTS cached (ip TEXT, data TEXT);");
+cache.exec("CREATE TABLE IF NOT EXISTS cached (ip TEXT, data TEXT, UNIQUE(ip));");
 
 module.exports = sys => (q, s, n) => {
   const ip = q.headers["x-forwarded-for"]?.split(",")[0] || q.socket.address().address;
   const wl = sys.prepare("SELECT ip FROM ip_white WHERE ip = ?;");
+  const ib = sys.prepare("SELECT ip FROM ip_block WHERE ip = ?;");
   const bl = sys.prepare("SELECT name FROM isp_block;").all().map(i => i.name);
   const ch = cache.prepare("SELECT data FROM cached WHERE ip = ?").get(ip);
 
   q.bip = false;
-  if (wl.get(ip)) return n();
+  if (wl.get(ip) || ib.get(ip)) return n();
   if (ch) {
     if (bl.find(n => ch.data.includes(n))) q.bip = true;
     return n();
@@ -21,7 +22,11 @@ module.exports = sys => (q, s, n) => {
     if (err) return n();
     if (bl.find(n => data.includes(n))) q.bip = true;
 
-    cache.prepare("INSERT INTO cached VALUES (?, ?);").run(ip, data);
+    try {
+      cache.prepare("INSERT INTO cached VALUES (?, ?);").run(ip, data);
+    } catch (err) {
+      // That's basically it.
+    }
     return n();
   });
 };
